8f84ddf3a6db09d17d43e6d0e15fc8e896ba6235
Victor Villazon <victorzn23@gmail.com>
2023-06-30 23:47:05 -0500

    semana05, dia03, Intro de JS - Gracias Mariana

---
+++ semana05/dia02-intro-js/app.js
@@ -0,0 +1,84 @@
+console.log("Hola mundo")
+console.log('Hola G14')
+console.log(2+2)
+
+
+// Tipos de datos en javascript
+// Number => operadores aritméticos
+console.log(100 + 4);
+console.log(100 * 4);
+console.log(100/4);
+// módulo => residuo
+
+console.log(typeof(10%3));
+
+// string = operador unitario
+console.log(typeof("Hola, mi nombre es Mariana"));
+
+// typeof()
+
+// Booleans => operadores comparativos (>, <, =)
+
+// true(verdadero)
+// false(false)
+
+console.log(typeof(3 < 5));
+console.log(10<15);
+
+// CONVERSIÓN AUTOMÁTICA => COERCIÓN DE TIPO
+
+console.log('5'-1);
+console.log(typeof('5'+1));
+
+// VARIABLES
+
+console.log('Mariana');
+
+let age = 18;
+
+let nombre 
+nombre = 'Gianmarco'
+console.log(nombre);
+
+nombre = 'Cesar'
+console.log(nombre);
+
+nombre = 'Wilson'
+console.log(nombre);
+
+
+const pais = 'peru'
+console.log(pais);
+
+
+
+
+let pais1 = 'Ecuador'
+pais1 = 'Holanda'
+
+console.log(pais1)
+
+// REGLAS PARA NOMBRAR VARIABLES
+// 1° No debe incluis espacios. 
+// let age of friends 
+// 2° No pueden ser palabras reservadas
+// let function = 'funcion'
+// 3° No pueden comenzar con números
+// let age2 = 25
+// 4° No pueden contener signos de puntuación ($ y _)
+// let abrir_puerta = 'tatata'
+
+// SUGERENCIAS
+// 1° Convención camelCase
+// PascalCase => FirstName
+// snake_case => first_name
+
+let firstName = 'Wilson'
+console.log(firstName);
+
+// 2° utilizar nombres en ingles
+let age5 = 25
+
+// 3° utilizar nombres desccriptivos
+
+let name5 = 'Christopher'
