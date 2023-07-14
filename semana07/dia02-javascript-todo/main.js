2f97ad26387da0a6267405da633a5803f517fef3
Victor Villazon <victorzn23@gmail.com>
2023-07-13 21:10:58 -0500

    semana07, dia02, empezando un nuevo ejercicio de lista de tareas parte 2 usando el DOM

--- semana07/dia02-javascript-todo/main.js
+++ semana07/dia02-javascript-todo/main.js
@@ -7,11 +7,44 @@ taskAdd.addEventListener('click', function(event) {
 
   // const button = document.createElement('button')
   // button.innerText = 'hola'
+
+  if (taskInput.value === '') {
+    alert('El campo es requerido')
+    return
+  }
   
   const li = document.createElement('li')
+
+  let checkbox = document.createElement('input')
+  checkbox.setAttribute('type', 'checkbox')
+  li.appendChild(checkbox)
+
+  let span = document.createElement('span')
+  span.innerText  = taskInput.value
+  li.appendChild(span)
+
+  // TODO: añadir un botón al li con el texto 'Borrar'
+  let button = document.createElement('button')
+  button.innerText = 'Borrar'
+  li.appendChild(button)
   
-  li.innerText = taskInput.value
+  // li.innerText = taskInput.value
 
   // document.body.appendChild(button)
   taskList.appendChild(li)
+
+  taskInput.value = ''
+})
+
+taskList.addEventListener('click', function(event) {
+  console.log(event)
+  const target = event.target
+
+  if (target.tagName === 'INPUT' && target.type === 'checkbox') {
+    target.classList.toggle('checked')
+  }
+
+  if (target.tagName === 'BUTTON') {
+    target.parentElement.remove()
+  }
 })
