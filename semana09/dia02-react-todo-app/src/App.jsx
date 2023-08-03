093b9c10fad41d23b65a0747e316e7f50c1ca44d
Victor Villazon <victorzn23@gmail.com>
2023-08-02 19:30:58 -0500

    semana09, dia02, terminando todo

--- semana09/dia02-react-todo-app/src/App.jsx
+++ semana09/dia02-react-todo-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { TrashIcon } from "./components/Icons.jsx"
 
 function App() {
 
@@ -44,6 +45,8 @@ function App() {
     
     console.log('Estoy funcionando....')
 
+    if (input === '') return
+
     const newTodo = {
       "id": crypto.randomUUID(),
       "title": input,
@@ -51,6 +54,8 @@ function App() {
     }
 
     setTodos([...todos, newTodo])
+
+    setInput('')
   }
 
   const handleCompleted = (event) => {
@@ -70,6 +75,28 @@ function App() {
     setTodos(newTodos)
   }
 
+  const handleRemoveTodo = (event) => {
+    const idSelected = event.target.dataset.id
+
+    const newTodos = todos.filter(todo => todo.id !== idSelected)
+
+    console.log(newTodos)
+
+    setTodos(newTodos)
+  }
+
+  const completedTodos = () => {
+    const completed = todos.filter(todo => todo.completed === true)
+
+    return completed.length
+  }
+
+  const handleClearTodos = (event) => {
+    const newTodos = todos.filter(todo => todo.completed === false)
+
+    setTodos(newTodos)
+  }
+
   return (
     <>
       <main className="w-full max-w-sm mx-auto mt-10 rounded-lg bg-yellow-100 border border-yellow-600 p-4 shadow-lg">
@@ -80,18 +107,27 @@ function App() {
             type="text"
             className="w-full border my-3 p-3"
             placeholder="¿Qué deseas hacer hoy?"
+            required
+            value={input}
             onChange={handleChange}
           />
         </form>
+
+        <div className="flex justify-between">
+          <span className="">{completedTodos()} / {todos.length}</span>
+          <button
+            className="bg-blue-500 rounded-lg px-2 py-1 text-white"
+            onClick={handleClearTodos}
+          >
+            Limpiar tareas completadas
+          </button>
+        </div>
 
         <section className="mt-5">
           <ul className="flex flex-col gap-3">
             {todos.map(todo => {
               return (
-                <li
-                  key={todo.id}
-                  className={`flex text-stone-900 ${todo.completed ? 'line-through' : ''}`}
-                >
+                <li key={todo.id} className="flex">
                   <input
                     type="checkbox"
                     className="mr-4"
@@ -99,7 +135,20 @@ function App() {
                     checked={todo.completed}
                     onChange={handleCompleted}
                   />
-                  <span className="pr-3">{todo.title}</span>
+                  <div className="flex justify-between items-center w-full">
+                    <div
+                      className={`text-stone-900 ${todo.completed ? 'line-through' : ''}`}
+                    >
+                      {todo.title}
+                    </div>
+                    <button
+                      className="font-bold bg-red-300 rounded-lg px-2 py-2 text-white"
+                      data-id={todo.id}
+                      onClick={handleRemoveTodo}
+                    >
+                      ❌
+                    </button>
+                  </div>
                 </li>
               )
             })}
