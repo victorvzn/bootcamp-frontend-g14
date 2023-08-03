4d1f0c4030af26b5dc86eda6984e917fb71fa2e5
Victor Villazon <victorzn23@gmail.com>
2023-08-02 21:56:22 -0500

    semana10, dia01, refactor terminado

--- semana10/dia01-react-todo-app-refactor/src/App.jsx
+++ semana10/dia01-react-todo-app-refactor/src/App.jsx
@@ -7,7 +7,33 @@ import TodoList from "./components/TodoList"
 
 function App() {
 
-  const DEFAULT_TODOS = []
+  const DEFAULT_TODOS = [
+    {
+      "id": '1',
+      "title": "delectus aut autem",
+      "completed": true
+    },
+    {
+      "id": '2',
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "id": '3',
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "id": '4',
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "id": '5',
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    }
+  ]
 
   const [todos, setTodos] = useState(DEFAULT_TODOS)
 
