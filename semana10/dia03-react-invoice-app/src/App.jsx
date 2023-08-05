0f9129a9c904acc03cece0e0a595974c61872c8d
Victor Villazon <victorzn23@gmail.com>
2023-08-04 20:08:00 -0500

    semana10, dia03, listando invoices del api de invoices

--- semana10/dia03-react-invoice-app/src/App.jsx
+++ semana10/dia03-react-invoice-app/src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const url = 'http://localhost:3000/invoices'
+
+  const [invoices, setInvoices] = useState([])
+
+  useEffect(() => {
+    const fetchInvoices = async () => {
+      const response = await fetch(url)
+
+      const data = await response.json()
+
+      console.log(data)
+
+      setInvoices(data)
+    }
+
+    fetchInvoices()
+  }, [])
+
   return (
     <>
       <h1 className="text-4xl">Invoices</h1>
+
+      {/* {JSON.stringify(invoices)} */}
+
+      {invoices.map(invoice => {
+
+        return (
+          <>
+            <h1>{invoice.id}</h1>
+            <span>{invoice.bill.to.client.name}</span>
+          </>
+        )
+
+      })}
     </>
   )
 }
