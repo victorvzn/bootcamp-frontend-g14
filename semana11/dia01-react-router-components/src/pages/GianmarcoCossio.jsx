8b661b65db82cc43dbf07fd9de57fd625bf2c12b
gianmarcoCossioArmijos <117977501+gianmarcoCossioArmijos@users.noreply.github.com>
2023-08-09 20:34:10 -0500

    Page GianmarcoCossio creado

---
+++ semana11/dia01-react-router-components/src/pages/GianmarcoCossio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GianmarcoCossio = () => {
+  return (
+    <>
+    <div>
+        <h1>Hello Fellas!</h1>
+    </div>
+    </>
+  )
+}
+
+export default GianmarcoCossio
