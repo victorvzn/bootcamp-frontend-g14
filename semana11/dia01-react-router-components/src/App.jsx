38406bcfe869dbb2eaf99d5c37dcda04ac2b69f7
Victor Villazon <victorzn23@gmail.com>
2023-08-09 20:01:53 -0500

    semana11, dia01, usando BrowserRouter, Routes y Route

--- semana11/dia01-react-router-components/src/App.jsx
+++ semana11/dia01-react-router-components/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
