21a4c0c3a5f394be50da0b21ac2d60217df6b217
Victor Villazon <victorzn23@gmail.com>
2023-08-10 20:31:15 -0500

    semana11, dia02, maquetando grilla de pokemons

--- semana11/dia02-react-router-hooks/src/pages/Home.jsx
+++ semana11/dia02-react-router-hooks/src/pages/Home.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  const fetchPokemons = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const data = await response.json()
+    setPokemons(data.results)
+  }
+
+  useEffect(() => { 
+    fetchPokemons()
+  }, [])
+
   return (
-    <div>Home</div>
+    // <div>{JSON.stringify(pokemons)}</div>
+    <section className="grid grid-cols-4 w-10/12 mx-auto mt-5 gap-4">
+      {
+        pokemons.map(pokemon => {
+          return (
+            <article
+              key={pokemon.url}
+              className="capitalize text-center border rounded-lg bg-yellow-600 p-4"
+            >
+              {pokemon.name}
+            </article>
+          )
+        })
+      }
+    </section>
   )
 }
 
