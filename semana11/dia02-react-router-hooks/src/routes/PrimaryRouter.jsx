be525d2b04fa3e964410d938ffcfb00a75d8bbd7
Victor Villazon <victorzn23@gmail.com>
2023-08-10 22:06:00 -0500

    semana11, dia02, refactorizando componentes PrimaryRouter y App

--- semana11/dia02-react-router-hooks/src/routes/PrimaryRouter.jsx
+++ semana11/dia02-react-router-hooks/src/routes/PrimaryRouter.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import PrimaryLayout from './layouts/PrimaryLayout'
+import PrimaryLayout from '../layouts/PrimaryLayout'
 
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import PokemonDetail from './pages/PokemonDetail'
-import ErrorPage from './pages/ErrorPage'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import PokemonDetail from '../pages/PokemonDetail'
+import ErrorPage from '../pages/ErrorPage'
 
 export const PrimaryRouter = createBrowserRouter([
   {
