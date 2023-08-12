1672a3de84c979883af05ed220d84dc4f58a3be8
Victor Villazon <victorzn23@gmail.com>
2023-08-11 20:28:19 -0500

    semana11, dia03, avanzando con la pagina InvoiceDetailPage.jsx y su sección inferior

--- semana11/dia02-react-invoice-app-continuacion/src/pages/InvoiceDetailPage.jsx
+++ semana11/dia02-react-invoice-app-continuacion/src/pages/InvoiceDetailPage.jsx
@@ -30,7 +30,7 @@ const InvoiceDetailPage = () => {
             className='text-emerald-100'
             label="Pending"
             background='bg-emerald-400/20'
-            textColor='text-emeraldr-400'
+            textColor='text-emerald-400'
           />
         </div>
 
@@ -55,6 +55,83 @@ const InvoiceDetailPage = () => {
           </BaseButton>
         </div>
       </header>
+
+      <section className="w-[940px] mx-auto bg-[#1f213a] mt-5 p-12 rounded-lg text-white">
+        <header className="flex justify-between">
+          <div className="text-3xl">
+            <span className="text-slate-400">#</span>
+            <span className="font-extrabold">XM941</span>
+            <div className="text-xl mt-1">
+              Grhaphic design
+            </div>
+          </div>
+
+          <div className="text-base">
+            <div>Line1</div>
+            <div>Line2</div>
+            <div>Line3</div>
+            <div>Line4</div>
+          </div>
+        </header>
+
+        <section className="grid grid-cols-3">
+          <div className="flex flex-col gap-10">
+            <div className="">
+              <div className="text-lg mt-1">Invoice Date</div>
+              <div className="text-xl mt-1 font-extrabold">21 Agu 2021</div>
+            </div>
+            <div className="">
+              <div className="text-lg mt-1">Payment Date</div>
+              <div className="text-xl mt-1 font-extrabold">21 Agu 2021</div>
+            </div>
+          </div>
+          <div>
+            <div className="text-lg mt-1">Bill To</div>
+            <div className="text-xl mt-1 font-extrabold">Alex Grim</div>
+            <div className="text-base mt-1">Line 1</div>
+            <div className="text-base mt-1">Line 2</div>
+            <div className="text-base mt-1">Line 3</div>
+            <div className="text-base mt-1">Line 4</div>
+          </div>
+          <div>
+            <div className="text-lg mt-1">Send To</div>
+            <div className="text-xl mt-1 font-extrabold">alexgrim@mail.com</div>
+          </div>
+        </section>
+
+        <table className="bg-[#252945] w-full rounded-lg mt-10">
+          <thead>
+            <tr>
+              <td className="p-6 text-left">Item Name</td>
+              <td className="p-6">QTY.</td>
+              <td className="p-6">Price</td>
+              <td className="p-6">Total</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="px-6 py-3">xyz</td>
+              <td className="px-6 py-3">xyz</td>
+              <td className="px-6 py-3">xyz</td>
+              <td className="px-6 py-3">xyz</td>
+            </tr>
+            <tr>
+              <td className="px-6 py-3">xyz</td>
+              <td className="px-6 py-3">xyz</td>
+              <td className="px-6 py-3">xyz</td>
+              <td className="px-6 py-3">xyz</td>
+            </tr>
+          </tbody>
+          <tfoot className="bg-[#0b0e16]">
+            <tr>
+              <td className="px-6 py-10">xyz</td>
+              <td className="px-6 py-10">xyz</td>
+              <td className="px-6 py-10">xyz</td>
+              <td className="px-1 py-10">xyz</td>
+            </tr>
+          </tfoot>
+        </table>
+      </section>
     </>
   )
 }
