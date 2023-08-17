03e250f11eea05d5e467b9313906f16bb8f0aef9
Victor Villazon <victorzn23@gmail.com>
2023-08-16 22:41:38 -0500

    semana12, dia01, separando las rutas del componente app

--- semana12/dia01-react-budget-app-custom-hooks-context/src/App.jsx
+++ semana12/dia01-react-budget-app-custom-hooks-context/src/App.jsx
@@ -1,21 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import LayoutBase from './layouts/LayoutBase'
-
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-
 export const App = () => {
   return (
-    <BrowserRouter>
-      <LayoutBase>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </LayoutBase>
-    </BrowserRouter>
+    
   ) 
 }
---
+++ semana12/dia01-react-budget-app-custom-hooks-context/src/router/Router.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import LayoutBase from '../layouts/LayoutBase'
+
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <LayoutBase>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </LayoutBase>
+    </BrowserRouter>
+  )
+}
+
+export default Router
