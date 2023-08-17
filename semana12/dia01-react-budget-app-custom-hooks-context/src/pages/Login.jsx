a5a3a7a68714d4cd08fb6bfeb7f5bf8ef8129f95
Victor Villazon <victorzn23@gmail.com>
2023-08-16 20:26:05 -0500

    semana12, dia01, añadiendo maquetación de la ruta login

--- semana12/dia01-react-budget-app-custom-hooks-context/src/pages/Login.jsx
+++ semana12/dia01-react-budget-app-custom-hooks-context/src/pages/Login.jsx
@@ -1,6 +1,32 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <form className="w-96 mx-auto">
+      <h1 className="text-center text-3xl mb-8">Budget App - Login</h1>
+
+      <div className="flex flex-col gap-4 bg-slate-200 p-8 rounded-lg shadow-lg">
+        <label className="font-medium">
+          Email
+          <input
+            type="text"
+            placeholder="jhon@mail.com"
+            className="border w-full p-3"
+          />
+        </label>
+        <label className="font-medium">
+          Password
+          <input
+            type="text"
+            placeholder="jhon@mail.com"
+            className="border w-full p-3"
+          />
+        </label>
+        <input
+          type="submit"
+          value="Login"
+          className="w-full bg-amber-300 p-3 font-medium"
+        />
+      </div>
+    </form>
   )
 }
 
