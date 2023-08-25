fb5513bc3163c6c0bb3402b5f6823f8df6756412
Victor Villazon <victorzn23@gmail.com>
2023-08-24 21:05:52 -0500

    creando el slice card con su estado inicial

---
+++ semana13/dia02-react-redux-shopping-cart-app/src/store/cart/slice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+  "id": 1,
+  "title": "iPhone 9",
+  "description": "An apple mobile which is nothing like apple",
+  "price": 549,
+  "discountPercentage": 12.96,
+  "rating": 4.69,
+  "stock": 94,
+  "brand": "Apple",
+  "category": "smartphones",
+  "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  "images": [
+  "https://i.dummyjson.com/data/products/1/1.jpg",
+  "https://i.dummyjson.com/data/products/1/2.jpg",
+  "https://i.dummyjson.com/data/products/1/3.jpg",
+  "https://i.dummyjson.com/data/products/1/4.jpg",
+  "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+  ]
+  },
+  {
+  "id": 2,
+  "title": "iPhone X",
+  "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+  "price": 899,
+  "discountPercentage": 17.94,
+  "rating": 4.44,
+  "stock": 34,
+  "brand": "Apple",
+  "category": "smartphones",
+  "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+  "images": [
+  "https://i.dummyjson.com/data/products/2/1.jpg",
+  "https://i.dummyjson.com/data/products/2/2.jpg",
+  "https://i.dummyjson.com/data/products/2/3.jpg",
+  "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+  ]
+  },
+  {
+  "id": 3,
+  "title": "Samsung Universe 9",
+  "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+  "price": 1249,
+  "discountPercentage": 15.46,
+  "rating": 4.09,
+  "stock": 36,
+  "brand": "Samsung",
+  "category": "smartphones",
+  "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+  "images": [
+  "https://i.dummyjson.com/data/products/3/1.jpg"
+  ]
+  }
+]
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState, // Object, Array, String, Number, Boolean
+  reducers: {
+    // addToCart
+    // removeFromCart
+  }
+})
+
+export default cartSlice.reducer // Exportando a los reducers del slice
