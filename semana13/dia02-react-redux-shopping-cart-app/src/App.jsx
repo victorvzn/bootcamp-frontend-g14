c68e07c8b1c87ef1b81fa4b3ef2a5edf9c28dda3
Victor Villazon <victorzn23@gmail.com>
2023-08-24 20:13:01 -0500

    haciendo fectch de dummyjson usando axios

--- semana13/dia02-react-redux-shopping-cart-app/src/App.jsx
+++ semana13/dia02-react-redux-shopping-cart-app/src/App.jsx
@@ -1,13 +1,36 @@
 import { Box, Container } from '@mui/material'
 
 import Header from './components/Header'
+import ProductList from './components/ProductList'
+
+import { useEffect, useState } from 'react'
+
+import axios from 'axios'
 
 function App() {
+  const [products, setProducts] = useState()
+
+  const fetchProducts = async () => {
+    const BASE_URL = 'https://dummyjson.com/products'
+    const response = await axios.get(BASE_URL)
+    console.log(response.data)
+    return response.data
+  }
+
+  useEffect(() => {
+    fetchProducts()
+      .then(data => setProducts(data.products))
+  }, [])
+
   return (
     <>
       <Container>
         <Box sx={{ my: 4 }}>
           <Header />
+        </Box>
+
+        <Box>
+          <ProductList products={products} />
         </Box>
       </Container>
     </>
---
+++ semana13/dia02-react-redux-shopping-cart-app/src/components/ProductList.jsx
@@ -0,0 +1,10 @@
+const ProductList = ({ products }) => {
+  return (
+    <div>
+      ProductList
+      {JSON.stringify(products)}
+    </div>
+  )
+}
+
+export default ProductList
